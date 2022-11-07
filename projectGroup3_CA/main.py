95a1a05d117963b4b0563b6ec21def2e1f750680
Chirag20047 <chirag20047@iiitd.ac.in>
2022-11-07 20:20:40 +0530

    Flow Of Control Decided

--- projectGroup3_CA/main.py
+++ projectGroup3_CA/main.py
@@ -44,6 +44,26 @@ class RegisterFile:
 def main():
     binary = open('testBinary.txt', 'r')
     output = open('logFile.txt', 'w')
+    clock = Clock(0)    # Clock obj starting from 0
+    while True:
+        # all 5 stages manipulated
+        # Fetch: instruction from instructionMemory obj
+        # Decode :  extract useful group of bits from the fetched instruction
+        # Xecute : execute and store the arithmetic operations in a temp variable
+        # Memory : load or store instruction manipulation
+        # WriteBack : update register file
+        # PTR :
+        # 1) Each stage object will converse with prev stage object .
+        # 2) Each stage object will converse with next stage as well : to avoid structural hazard
+        #     : use of flag (busy)
+        # 3) Data Hazard : RAW WAR WAW [TO BE DECIDED !!!]
+        #     : current stage destination register should not clash with prev
+        #     :
+        #     :
+        # Display the register file
+        # clock += 1
+
+        pass
 
     output.close()
     binary.close()
